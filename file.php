d4a5745354f57eda84193d4f7b04fc2287571c1d
duke995 <beliar1995@gmail.com>
2017-04-06 12:02:59 +0200

    Modifica per salvare sul file

--- file.php
+++ file.php
@@ -1,7 +1,17 @@
 <?php
-    $nome=$_POST["name"];
+   /* $nome=$_POST["name"];
     $cognome = $_POST["surname"];
     $data = $_POST["date"];
     $password = $_POST["password"];
-    print("$nome, $cognome, $data, $password");
+    print("$nome, $cognome, $data, $password");*/
+
+$file = "file.txt";
+
+
+$fp = fopen($file, "a");
+
+
+fputs($fp, htmlspecialchars($_POST["name"]) . " " . htmlspecialchars($_POST["surname"]) . " " . htmlspecialchars($_POST["date"]) . " " . htmlspecialchars($_POST["password"]). "\r\n");
+
+fclose($fp);
 ?>
